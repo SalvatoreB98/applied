0824a78558423b33e24f0060fa51682b34c6ec81
Salvatore Butera <salvatore.butera@nttdata.com>
2023-05-13 13:50:10 +0200

    Dots navigation

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -38,8 +38,6 @@ window.addEventListener("load", () => {
       let width = 0;
       for (let i = 0; i < index; i++) {
         width += images[i].offsetWidth + 20;
-        console.log(images[i].offsetWidth)
-        console.log(width)
       }
       return width;
     }
@@ -61,10 +59,21 @@ window.addEventListener("load", () => {
     function createDots(){
       let htmlString = ''
       imgArray.forEach((img, i)=>{
-        htmlString += `<button class="circle outline"></div> `;
+        htmlString += `<button class="circle outline" data-value=${i}></div> `;
       })
       dotsContainer.innerHTML = htmlString;
-      return document.querySelectorAll('.dots-container .circle') 
+      const dotsHtmlElementArray = document.querySelectorAll('.dots-container .circle');
+      // Crea dei dots con event listener in modo che le immagini siano navigabili tramite il click su di essi
+      dotsHtmlElementArray.forEach((dot)=>{
+          dot.addEventListener("click",(e)=>{
+          const dotIndex = parseInt(e.target.getAttribute("data-value", 10));
+          index = dotIndex;
+          indexChange();
+          console.log(index)
+          scroll(scroller);
+        })
+      })
+      return dotsHtmlElementArray
     }
 
     function moveSelectedDots(){
