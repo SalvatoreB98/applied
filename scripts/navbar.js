8f2e142039a9ac7c4bb39d66cfa91727ffb09d2e
Salvatore Butera <salvatore.butera@nttdata.com>
2023-05-18 11:05:05 +0200

    Fix mobile responsiveness and navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,10 +2,16 @@ window.addEventListener("load",()=>{
     let isMenuOpen = false;
     const burger = document.querySelector(".burger");
     const menuMobile = document.querySelector(".mobile-menu"); 
-    console.log()
+    const navbar = document.querySelector("navbar")
+    let prevScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    window.addEventListener("scroll", handleScroll);
+    burger.addEventListener("click", changeMenuStatus)
     
-    burger.addEventListener("click",()=> {;
-        changeMenuStatus();
+    menuMobile.addEventListener("click",()=>{
+        if(isMenuOpen){
+            changeMenuStatus();
+        }
     })
 
     function changeMenuStatus(){
@@ -16,10 +22,15 @@ window.addEventListener("load",()=>{
         }
         isMenuOpen = !isMenuOpen;
     }
-    
-    menuMobile.addEventListener("click",()=>{
-        if(isMenuOpen){
-            changeMenuStatus();
+
+    function handleScroll() {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScroll > prevScroll && currentScroll > 100) {
+            navbar.style.height = '0px';
+            navbar.style.overflow = 'hidden'
+        } else if (currentScroll < prevScroll ) {
+            navbar.style.height = '80px';
         }
-    })
+        prevScroll = currentScroll;
+    }
 })
